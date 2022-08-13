a0fdd60f79b5e3ae87a8abfcad35f463b024170c
SergDeikun <dsv2807@gmail.com>
2022-08-13 22:38:50 +0300

    add redux-persist

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,9 @@
-import { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
 import { Container } from './App.styled';
 import ContactForm from '../contactForm/contactForm';
 import ContactsList from '../contactsList/contactsList';
 import Filter from '../filter/filter';
 
 const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    return JSON.parse(window.localStorage.getItem('contacts')) ?? [];
-  });
-
-  useEffect(() => {
-    window.localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
   return (
     <Container>
       <h1>Phonebook</h1>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,38 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import { filterReducer } from './filter';
-import { itemsReducer } from './contacts';
+import contacts from './contacts';
+
+const persistConfig = {
+  key: 'contacts',
+  version: 1,
+  storage,
+};
+
+const persistedContactsReducer = persistReducer(persistConfig, contacts);
 
 export const store = configureStore({
   reducer: {
     filter: filterReducer,
-    contacts: itemsReducer,
+    contacts: persistedContactsReducer,
   },
+
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+export const persistor = persistStore(store);
